840d6aed217b8e984ca4e10806b0c20b8aa5ae0e
Thomas Vinadelle <thomasvinadelle@live.fr>
2019-01-07 12:03:23 +0100

    Add ceiling + create RotatedDoor

--- Source/BuildingEscape/RotateDoor.cpp
+++ Source/BuildingEscape/RotateDoor.cpp
@@ -1,9 +1,10 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-#include "GetPositionComponent.h" 
+#include "RotateDoor.h"
+
 
 // Sets default values for this component's properties
-UGetPositionComponent::UGetPositionComponent()
+URotateDoor::URotateDoor()
 {
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
@@ -14,23 +15,23 @@ UGetPositionComponent::UGetPositionComponent()
 
 
 // Called when the game starts
-void UGetPositionComponent::BeginPlay()
+void URotateDoor::BeginPlay()
 {
 	Super::BeginPlay();
 
-	FVector ActorPos = GetOwner()->GetTransform().GetLocation();
-	FName ActorName = GetOwner()->GetFName();
+	FRotator Rotation(0, -90, 0);
+	GetOwner()->SetActorRotation(Rotation);
 
-	UE_LOG(LogTemp, Warning, TEXT("%s.Location(%s)"), *ActorName.ToString(), *ActorPos.ToString());
 	// ...
-	
+
 }
 
 
 // Called every frame
-void UGetPositionComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+void URotateDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
 	// ...
 }
 
